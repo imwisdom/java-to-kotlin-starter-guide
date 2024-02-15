520b5dca3415f5a29de98a327f90126eaf1e0eb0
imwisdom <kjihye0340@naver.com>
2024-02-15 16:30:08 +0900

    코틀린에서 if/switch문 사용

---
+++ src/main/kotlin/com/imwisdom/lec05/Lec05Main.kt
@@ -0,0 +1,63 @@
+package com.imwisdom.lec05
+
+import java.lang.IllegalArgumentException
+
+fun main(){
+
+}
+fun validateScoreIsNotNegative(score: Int) {
+    if (score !in 0..100) {
+        throw IllegalArgumentException("${score}는 0보다 작을 수 없습니다.")
+    }
+}
+
+fun getPassOrFail(score: Int): String{
+    return if (score >= 90) {
+        "P"
+    } else {
+        "F"
+    }
+}
+
+fun getGrade(score: Int): String{
+    return if (score >= 90) {
+        "A"
+    } else if (score >= 80) {
+        "B"
+    } else if (score >= 70) {
+        "C"
+    } else {
+        "D"
+    }
+}
+
+fun getGradeWithSwitch(score: Int): String{
+    return when (score / 10){   //java의 switch
+        9 -> "A"
+        8 -> "B"
+        7 -> "C"
+        else -> "D"
+    }
+}
+
+fun startsWithA(obj: Any): Boolean{
+    return when (obj) {
+        is String -> obj.startsWith("A")
+        else -> false
+    }
+}
+
+fun judgeNumber(number: Int){
+    when (number) {
+        1, 0, -1 -> println("어디서 많이 본 숫자입니다.")
+        else -> println("1, 0, -1이 아닙니다")
+    }
+}
+
+fun judgeNumber2(number: Int){
+    when {
+        number == 0 -> println("주어진 숫자는 0입니다.")
+        number % 2 == 0 -> println("주어진 숫자는 짝수입니다.")
+        else -> println("주어지는 숫자는 홀수입니다.")
+    }
+}
